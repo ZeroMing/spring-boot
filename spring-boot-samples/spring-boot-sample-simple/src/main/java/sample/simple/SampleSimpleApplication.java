911c460adf22e745c3d5fc299c42548a27321cd3
ming_lee <1336942608@qq.com>
2019-12-28 20:47:33 +0800

    源码环境搭建

--- spring-boot-samples/spring-boot-sample-simple/src/main/java/sample/simple/SampleSimpleApplication.java
+++ spring-boot-samples/spring-boot-sample-simple/src/main/java/sample/simple/SampleSimpleApplication.java
@@ -23,6 +23,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SampleSimpleApplication implements CommandLineRunner {
 
